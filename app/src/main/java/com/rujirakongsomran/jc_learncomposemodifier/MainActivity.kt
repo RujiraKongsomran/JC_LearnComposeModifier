977123f1541a63c0911fc5185326adb1bffc3dcb
Rujira Kongsomran <rujirabiwberry@gmail.com>
2023-12-08 19:35:16 +0700

    Create TaroImage func

--- app/src/main/java/com/rujirakongsomran/jc_learncomposemodifier/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_learncomposemodifier/MainActivity.kt
@@ -3,12 +3,14 @@ package com.rujirakongsomran.jc_learncomposemodifier
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.rujirakongsomran.jc_learncomposemodifier.ui.theme.JC_LearnComposeModifierTheme
 
@@ -22,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
@@ -30,17 +32,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun TaroImage(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.taro),
+        contentDescription = "TaroImage",
+        contentScale = ContentScale.FillWidth,
         modifier = modifier
     )
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JC_LearnComposeModifierTheme {
-        Greeting("Android")
+
     }
 }
